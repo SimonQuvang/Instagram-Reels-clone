0c2df6aa7139494028a04f8f0c4950f2774a01f0
SimonQuvang <simon.quvang@gmail.com>
2020-12-19 05:42:09 +0100

    Initial setup
    Setting up the basis for the instagram reels feed clone.

---
+++ ig-reels-clone/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__top">
+        <img className="app__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+        alt=""></img>
+        <h1>Reels</h1>
+      </div>
+
+      <div className="app__videos">
+       
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
