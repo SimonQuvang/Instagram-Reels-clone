93a29ad6d5e1390b8a3220350e945835339e24c0
SimonQuvang <simon.quvang@gmail.com>
2020-12-19 15:32:51 +0100

    Small changes to the frontend setup

--- ig-reels-clone/src/App.js
+++ ig-reels-clone/src/App.js
@@ -1,19 +1,24 @@
-import './App.css';
+import "./App.css";
+import VideoCard from "./VideoCard";
 
 function App() {
   return (
     <div className="app">
       <div className="app__top">
-        <img className="app__logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
-        alt=""></img>
+        <img
+          className="app__logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+          alt=""
+        ></img>
         <h1>Reels</h1>
       </div>
 
       <div className="app__videos">
-       
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
+        <VideoCard />
       </div>
-      
     </div>
   );
 }
