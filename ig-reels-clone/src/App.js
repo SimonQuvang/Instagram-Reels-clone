0e9a7f280514c5343cc59ec330176e7765e7ca75
SimonQuvang <simon.quvang@gmail.com>
2020-12-21 22:03:59 +0100

    Added the video footer and header
    Wrote the basic component for video header and footer, together with basic css to set it up. 
    Encountered an issue with showing/passing videos. 
    Trying to resolve that.

--- ig-reels-clone/src/App.js
+++ ig-reels-clone/src/App.js
@@ -1,26 +1,36 @@
 import "./App.css";
 import VideoCard from "./VideoCard";
+import VideoFooter from "./VideoFooter";
 
 function App() {
-  return (
-    <div className="app">
-      <div className="app__top">
-        <img
-          className="app__logo"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
-          alt=""
-        ></img>
-        <h1>Reels</h1>
-      </div>
+    return (
+        <div className="app">
+            <div className="app__top">
+                <img
+                    className="app__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/768px-Instagram_logo_2016.svg.png"
+                    alt=""
+                ></img>
+                <h1>Reels</h1>
+            </div>
 
-      <div className="app__videos">
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-      </div>
-    </div>
-  );
+            <div className="app__videos">
+                <VideoCard
+                    channel="SimonQuvang"
+                    avatarSrc="https://weneedfun.com/wp-content/uploads/2016/07/Cute-Girl-Profile-pictures-For-Facebook-14.jpg"
+                    videoSrc={
+                        "https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                    }
+                    song="Test song by testy test"
+                    likes={509}
+                    shares={30}
+                />
+                <VideoFooter />
+                <VideoCard />
+                <VideoCard />
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- ig-reels-clone/src/VideoCard.js
+++ ig-reels-clone/src/VideoCard.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./VideoCard.css";
+import VideoHeader from "./VideoHeader";
+import VideoFooter from "./VideoFooter";
 
-function VideoCard() {
-  return (
-    <div className="video__card">
-      <video
-        className="video__player"
-        src="https://scontent-cph2-1.cdninstagram.com/v/t50.2886-16/109329692_651954692070420_7176758539549662205_n.mp4?efg=eyJ2ZW5jb2RlX3RhZyI6InZ0c192b2RfdXJsZ2VuLjcyMC5mZWVkLmRlZmF1bHQiLCJxZV9ncm91cHMiOiJbXCJpZ193ZWJfZGVsaXZlcnlfdnRzX290ZlwiXSJ9&_nc_ht=scontent-cph2-1.cdninstagram.com&_nc_cat=100&_nc_ohc=eIuQ4tgjSZ8AX9spiDu&vs=17935600450385003_902420066&_nc_vs=HBksFQAYJEdCdzloQVlVbk5RSDgxQUNBUDNibXh6ZTk1aGpia1lMQUFBRhUAAsgBABUAGCRHSnZ6aFFZdDhTa0ZiZjBBQU5KZVVRMmZZS0kxYmtZTEFBQUYVAgLIAQAoABgAGwAVAAAm1prjnqyV3D8VAigCQzMsF0BFIgxJul41GBJkYXNoX2Jhc2VsaW5lXzFfdjERAHXqBwA%3D&_nc_rid=d805b9a638&oe=5FE00D52&oh=de750a5c1bef7b7849dd00a1b771b2e6"
-        alt="IG reel video"
-        loop
-      />
-    </div>
-  );
+function VideoCard({ url, likes, shares, channel, avatarSrc, song }) {
+    const videoRef = useRef(null);
+    const [isVideoPlaying, setIsVideoPlaying] = useState(null);
+
+    const onVideoPress = () => {
+        if (isVideoPlaying) {
+            //Stop the video
+            videoRef.current.pause();
+            setIsVideoPlaying(false);
+        } else {
+            // Play the video
+            videoRef.current.play();
+            setIsVideoPlaying(true);
+        }
+    };
+    return (
+        <div className="video__card">
+            <VideoHeader />
+            <video ref={videoRef} onClick={onVideoPress} className="video__player" src={url} alt="IG reel video" loop />
+            <VideoFooter />
+        </div>
+    );
 }
 
 export default VideoCard;
